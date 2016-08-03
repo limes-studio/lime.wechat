875f3893e67a3c8de48f83d977964f0396c59d7e
suchenxiaoyu <chen.zhuang@hpe.com>
2016-08-03 17:15:01 +0800

    start

--- Lime.Weixin.Web/App_Start/BundleConfig.cs
+++ Lime.Weixin.Web/App_Start/BundleConfig.cs
@@ -10,20 +10,26 @@ namespace Lime.Weixin.Web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-            bundles.Add(new ScriptBundle("~/bootstrap").Include("~/Scripts/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/limejquery").Include(
+                        "~/Scripts/jquery-{version}.js"
+                        , "~/Scripts/bootstrap.min.js"
+                        , "~/admin-lte/js/app.min.js"
+                        , "~/Scripts/respond.min.js"
+                        , "~/Scripts/html5shiv.min.js"
+                        , "~/Areas/AppSystem/Scripts/layout.js"
+                        ));
+            //bundles.Add(new ScriptBundle("~/bootstrap").Include("~/Scripts/bootstrap.min.js"));
 
-            bundles.Add(new ScriptBundle("~/adminLteJs").Include("~/admin-lte/js/app.min.js"));
+            //bundles.Add(new ScriptBundle("~/adminLteJs").Include("~/admin-lte/js/app.min.js"));
 
-            bundles.Add(new ScriptBundle("~/compatibility").Include("~/Scripts/respond.min.js",
-                "~/Scripts/html5shiv.min.js"
-                ));
-            bundles.Add(new ScriptBundle("~/systemLayoutJs").Include("~/Areas/AppSystem/Scripts/layout.js"));
+            //bundles.Add(new ScriptBundle("~/compatibility").Include("~/Scripts/respond.min.js",
+            //    "~/Scripts/html5shiv.min.js"
+            //    ));
+            //bundles.Add(new ScriptBundle("~/systemLayoutJs").Include("~/Areas/AppSystem/Scripts/layout.js"));
 
             bundles.Add(new StyleBundle("~/adminLteStyle").Include(
                 "~/Content/bootstrap.min.css",
-               // "~/Content/css/font-awesome.min.css",
+                "~/Content/css/font-awesome.min.css",
                 "~/admin-lte/css/AdminLTE.min.css",
                 "~/admin-lte/css/skins/skin-blue.min.css",
                 "~/Content/css/ionicons.min.css",
